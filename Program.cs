e6364a832eb5a1211c498ae37ebeb9385d975ce8
Almaz-Sil <90396673+Almaz-Sil@users.noreply.github.com>
2022-03-30 14:46:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bars_group
+{
+    class BrsAnswer
+    {
+        public event EventHandler<char> OnKeyPressed;
+        public static ConsoleKeyInfo key;
+        public static void displayconsole(Object sender, char c) => Console.WriteLine($"\n Вы ввели: {key.KeyChar}");
+
+        public void Run()
+        {
+            OnKeyPressed += displayconsole;
+            while (true)
+            {
+                key = Console.ReadKey();                
+                if (key.KeyChar == 'c') break;
+
+                OnKeyPressed?.Invoke(this, key.KeyChar);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var Brs = new BrsAnswer();
+            Brs.Run();
+        }
+        public delegate void EventHandler();
+    }
+}
